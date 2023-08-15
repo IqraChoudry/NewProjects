acf3e3df89d40f7a9c8b0c67cbefa89aa365c603
Iqra <iqrachoudry92@gmail.com>
2023-08-14 23:54:15 -0400

    second

--- GitProject/src/gitproject/GitPractice.java
+++ GitProject/src/gitproject/GitPractice.java
@@ -3,8 +3,7 @@ package gitproject;
 public class GitPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				System.out.println("My name is Talentech");
 	}
 
 }
